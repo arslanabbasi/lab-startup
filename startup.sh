e1354cf1dd7faef7e5ce6174608bb199377f7db2
arslanabbasi <arslan.abbasi13@gmail.com>
2021-12-23 10:05:06 -0800

    Create startup.sh

---
+++ startup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+exec 3>&1 4>&2
+trap 'exec 2>&4 1>&3' 0 1 2 3
+exec 1>log.out 2>&1
+
+
+#sed -i "'s/10.126.106.15/$1/g" tap-values-dev-harbor.yaml
+
+#tanzu package installed update tap --package-name tap.tanzu.vmware.com --version 0.4.0 -n tap-install -f tap-values-dev-harbor.yaml
+
+echo "I am starting"
