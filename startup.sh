1149b641506af5ae2a803527ee2408cc00a6cb15
arslanabbasi <arslan.abbasi13@gmail.com>
2021-12-23 10:56:18 -0800

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -6,12 +6,12 @@
 
 sed -i "s/10.126.106.15/$1/g" /home/holuser/tap-values-dev-harbor.yaml
 
-tanzu package install tap -p tap.tanzu.vmware.com -v 0.4.0 --values-file tap-values-dev-harbor.yaml -n tap-install
+tanzu package install tap -p tap.tanzu.vmware.com -v 0.4.0 --values-file /home/holuser/tap-values-dev-harbor.yaml -n tap-install
 
 tanzu package installed list -A
 
 
-#tanzu package installed update tap --package-name tap.tanzu.vmware.com --version 0.4.0 -n tap-install -f tap-values-dev-harbor.yaml
+#tanzu package installed update tap --package-name tap.tanzu.vmware.com --version 0.4.0 -n tap-install -f /home/holuser/tap-values-dev-harbor.yaml
 echo "IP: $1"
 echo "I am starting1"
 sudo echo "testing2"
