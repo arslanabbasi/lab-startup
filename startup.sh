86e4f43d7c6a7ed97a3984cd1424ada8d7c9ae6a
arslanabbasi <arslan.abbasi13@gmail.com>
2021-12-23 10:39:11 -0800

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -12,6 +12,6 @@ exec 1>log.out 2>&1
 
 
 #tanzu package installed update tap --package-name tap.tanzu.vmware.com --version 0.4.0 -n tap-install -f tap-values-dev-harbor.yaml
-echo $1
+echo "IP: $1"
 echo "I am starting1"
 sudo echo "testing2"
