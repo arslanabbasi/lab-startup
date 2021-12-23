48c597379cc027ae7181537b04dc5fa17d08e61f
arslanabbasi <arslan.abbasi13@gmail.com>
2021-12-23 10:29:55 -0800

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -4,7 +4,12 @@ trap 'exec 2>&4 1>&3' 0 1 2 3
 exec 1>log.out 2>&1
 
 
-#sed -i "'s/10.126.106.15/$1/g" tap-values-dev-harbor.yaml
+sed -i "'s/10.126.106.15/$1/g" tap-values-dev-harbor.yaml
+
+#tanzu package install tap -p tap.tanzu.vmware.com -v 0.4.0 --values-file tap-values-dev-harbor.yaml -n tap-install
+
+#tanzu package installed list -A
+
 
 #tanzu package installed update tap --package-name tap.tanzu.vmware.com --version 0.4.0 -n tap-install -f tap-values-dev-harbor.yaml
 
