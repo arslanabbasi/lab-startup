091fabe5f3ad9f8b8bca1b4d9895d6e7a3a1690f
arslanabbasi <arslan.abbasi13@gmail.com>
2022-01-03 13:00:59 -0800

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -3,15 +3,15 @@
 #trap 'exec 2>&4 1>&3' 0 1 2 3
 #exec 1>log.out 2>&1
 
-if [ $1 == '' ]
-then
-  echo "Please provide IP as an argument"
-  exit 1
-fi
+#if [ $1 == '' ]
+#then
+#  echo "Please provide IP as an argument"
+#  exit 1
+#fi
 ip=$(curl myip.oc.vmware.com)
-echo $ip
-exit 1
-sed -i "s/10.126.106.15:/$1:/g" /home/holuser/tap-values-dev-harbor.yaml
+echo "My IP: $ip"
+
+sed -i "s/10.126.106.15:/$ip:/g" /home/holuser/tap-values-dev-harbor.yaml
 sed -i "s/10.126.106.15./192.168.0.2./g" /home/holuser/tap-values-dev-harbor.yaml
 
 tanzu package install tap -p tap.tanzu.vmware.com -v 0.4.0 --values-file /home/holuser/tap-values-dev-harbor.yaml -n tap-install
@@ -26,5 +26,5 @@ sed -i "s/32739/$port/g" /home/holuser/tap-values-dev-harbor.yaml
 
 tanzu package installed update tap --package-name tap.tanzu.vmware.com --version 0.4.0 -n tap-install -f /home/holuser/tap-values-dev-harbor.yaml
 
-echo "IP: $1"
+
 echo "Install Finished"
