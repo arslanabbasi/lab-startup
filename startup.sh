b9e7dce96326043f0a56741087d8b0380f919cea
arslanabbasi <arslan.abbasi13@gmail.com>
2022-01-12 08:25:25 -0800

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -7,16 +7,6 @@ exec 1>/home/holuser/install.log 2>&1
 export KUBECONFIG="/home/holuser/.kube/config"
 export HOME="/home/holuser"
 
-echo "pwd: $(pwd)"
-
-echo "whoami $(whoami)"
-
-echo "Home: $HOME"
-
-cat /home/holuser/.kube/config
-kubectl get pods --kubeconfig /home/holuser/.kube/config
-
-
 date
 counter=0
 while [ "True" ]
@@ -43,8 +33,8 @@ exit 0
 ip="192.168.0.2"
 echo "My IP: $ip"
 echo "Setting tap values yaml file"
-sed -i "s/10.126.106.15:/$ip:/g" /home/holuser/tap-values-dev-harbor.yaml
-sed -i "s/10.126.106.15./192.168.0.2./g" /home/holuser/tap-values-dev-harbor.yaml
+sed -i "s/10.126.106.14:/$ip:/g" /home/holuser/tap-values-dev-harbor.yaml
+#sed -i "s/10.126.106.14./192.168.0.2./g" /home/holuser/tap-values-dev-harbor.yaml
 
 echo "Installing TAP full profile"
 tanzu package install tap -p tap.tanzu.vmware.com -v 0.4.0 --values-file /home/holuser/tap-values-dev-harbor.yaml -n tap-install
@@ -55,7 +45,7 @@ port=$(kubectl get svc server -n tap-gui -o=jsonpath='{.spec.ports[].nodePort}')
 #echo $port
 
 echo "Updating TAP port: $port"
-sed -i "s/32739/$port/g" /home/holuser/tap-values-dev-harbor.yaml
+sed -i "s/31443/$port/g" /home/holuser/tap-values-dev-harbor.yaml
 #cat /home/holuser/tap-values-dev-harbor.yaml
 
 tanzu package installed update tap --package-name tap.tanzu.vmware.com --version 0.4.0 -n tap-install -f /home/holuser/tap-values-dev-harbor.yaml
